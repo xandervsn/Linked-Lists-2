ac88c67557046d5ad5dda93c4bc54a074706f817
UnoSchoolThingy <113061893+UnoSchoolThingy@users.noreply.github.com>
2023-01-17 14:26:17 -0800

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,28 @@
+#include "Node.h"
+// Probably want to include student thingy too 
+
+// Get next node
+Node* Node::getNext() {
+  return next;
+}
+
+// Get student from node 
+Student* Node::getStudent() {
+  return student;
+}
+
+// Set next node
+void Node::setNext(Node* n) {
+  next = n;
+}
+
+// Node constructor
+Node::Node(Student* s) {
+  student = s;
+  next = nullptr;
+}
+
+// Node destructor
+Node::~Node() {
+  delete student; // Make sure we get rid of the student too 
+}
