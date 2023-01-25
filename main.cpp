1c349b0cd0865bdbdd65649655252f03f613987a
U-BSD\412237 <412237@SHS-DGCSCS2.beaverton.k12.or.us>
2023-01-25 14:26:21 -0800

    skeleton

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main(){
-  
+  cout << "Please enter a command (ADD, PRINT, DLT, AVG, QUIT)" << endl;
+  char input[10];
+  cin >> input;
+  if(input == "ADD"){
+    
+  }else if(input == "PRINT"){
+    
+  }else if(input == "DLT"){
+    
+  }else if(input == "AVG"){
+    
+  }else if(input == "QUIT"){
+    
+  }
   
   return 0;
 }
